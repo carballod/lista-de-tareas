1d3f1b079b48ca81c9c7e20c1b04a044d4803fb3
carballod <109915531+carballod@users.noreply.github.com>
2023-03-21 22:31:29 -0300

    Agrega icon e imagen de header

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ const input = document.querySelector("input");
 const addBtn = document.querySelector(".btn-add");
 const ul = document.querySelector("ul");
 const empty = document.querySelector(".empty");
+const tasks = {};
 
-addBtn.addEventListener("click", (e)=>{
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-e.preventDefault();
+  const text = input.value;
 
-const text = input.value;
+  if (text in tasks){
+    input.value = "";
+    return;
+  } 
 
-if (text !== ""){
+  if (text.length < 2) return;
 
   const li = document.createElement("li");
   const p = document.createElement("p");
@@ -20,34 +25,33 @@ if (text !== ""){
   ul.appendChild(li);
   li.appendChild(addDeleteBtn());
 
+  // agregar la tarea al objeto
+  tasks[text] = true;
+
   input.value = "";
   empty.style.display = "none";
+});
 
-}
-
-}); 
-
-function addDeleteBtn () {
-
+function addDeleteBtn() {
   const deleteBtn = document.createElement("button");
 
   deleteBtn.textContent = "X";
   deleteBtn.className = "btn-delete";
 
-  deleteBtn.addEventListener("click", (e)=>{
-  
+  deleteBtn.addEventListener("click", (e) => {
     const item = e.target.parentElement;
-    
-    ul.removeChild(item);
-    
-    const items = document.querySelectorAll("li");
-    
-    if(items.length === 0) {
-                              empty.style.display = "block"
-}
-  });
- 
-  return deleteBtn;
 
+    ul.removeChild(item);
+
+    const items = document.querySelectorAll("li");
+
+    if (items.length === 0) {
+      empty.style.display = "block";
+    }
+
+    delete tasks[item.firstChild.textContent];
+  });
+
+  return deleteBtn;
 }
 
